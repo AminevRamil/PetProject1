0253a5f887c640088968a04f97829f79716122c8
Ramil Aminev <Aminev@lanit.ru>
2022-10-15 23:56:14 +0400

    Initial commit

---
+++ src/main/java/com/starbun/petproject1/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.starbun.petproject1;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(PetProject1Application.class);
+  }
+
+}
---
+++ src/test/java/com/starbun/petproject1/PetProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starbun.petproject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetProject1ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
