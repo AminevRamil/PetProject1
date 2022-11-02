dd25a4696a3c0a8830effa277863a8d67123311f
Ramil Aminev <Aminev@lanit.ru>
2022-11-03 02:29:03 +0400

    Перенос TelegramUser в пакет entity.

--- pet-impl/src/main/java/com/starbun/petproject1/entity/TelegramUser.java
+++ pet-impl/src/main/java/com/starbun/petproject1/entity/TelegramUser.java
@@ -1,12 +1,14 @@
-package com.starbun.petproject1.domain;
+package com.starbun.petproject1.entity;
 
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+@Entity
 @Getter
 @Setter
 @Table(name = "telegram_users")
@@ -27,5 +29,5 @@ public class TelegramUser {
   private String username;
 
   @Column(name = "is_actual")
-  private String isActual;
+  private Boolean isActual;
 }
