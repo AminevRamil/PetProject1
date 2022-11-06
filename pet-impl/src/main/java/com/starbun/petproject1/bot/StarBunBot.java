852f3bc69904201843ee4e49bbc3a711153965ba
Ramil Aminev <Aminev@lanit.ru>
2022-11-06 23:21:47 +0400

    Добавлено меню команд при старте приложения

--- pet-impl/src/main/java/com/starbun/petproject1/bot/StarBunBot.java
+++ pet-impl/src/main/java/com/starbun/petproject1/bot/StarBunBot.java
@@ -7,7 +7,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -31,10 +35,30 @@ public class StarBunBot extends TelegramLongPollingCommandBot {
   @PostConstruct
   public void init() {
     commandList.forEach(this::register);
+
+    initCommandMenu();
   }
 
   @Override
   public void processNonCommandUpdate(Update update) {
     updateProcessor.process(this, update);
   }
+
+  /**
+   * Заполнение меню команд для удобного просмотра пользователем.
+   *
+   * @implNote Добавить разный список команд для разных видов чатов,
+   * или ограничить использование бота в некоторых типах чата.
+   */
+  private void initCommandMenu() {
+    List<BotCommand> botCommandList = commandList.stream()
+        .map(basicCommand -> new BotCommand(basicCommand.getCommandIdentifier(), basicCommand.getDescription()))
+        .toList();
+    try {
+      this.execute(new SetMyCommands(botCommandList, new BotCommandScopeDefault(), null));
+    } catch (TelegramApiException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
 }
