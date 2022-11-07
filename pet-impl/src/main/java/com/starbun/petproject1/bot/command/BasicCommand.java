1fb6bc8490f42e7dfcfc18178967e14f3780de55
Ramil Aminev <Aminev@lanit.ru>
2022-11-08 01:22:45 +0400

    Обновление базового интерфейса обработчика обновлений и базового класса команд. Оборачивание проверяемых исключений непроверяемыми.

--- pet-impl/src/main/java/com/starbun/petproject1/bot/command/BasicCommand.java
+++ pet-impl/src/main/java/com/starbun/petproject1/bot/command/BasicCommand.java
@@ -2,6 +2,11 @@ package com.starbun.petproject1.bot.command;
 
 import lombok.extern.slf4j.Slf4j;
 import org.telegram.telegrambots.extensions.bots.commandbot.commands.DefaultBotCommand;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.bots.AbsSender;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.io.Serializable;
 
 /**
  * Маркер, по которому Spring автоматически собирает все команды, для последующей регистрации в боте.
@@ -12,8 +17,26 @@ import org.telegram.telegrambots.extensions.bots.commandbot.commands.DefaultBotC
 @Slf4j
 public abstract class BasicCommand extends DefaultBotCommand {
 
+  /**
+   * Необходимость заполнить эти два поля, делает невозможным создания простого конструктора
+   * для инъекции зависимостей через ломбок, используя AllArgsConstructor. Поэтому следует создать
+   * свой конструктор с получением и заданием необходимых зависимостей, но предварительно вызвав
+   * Данный родительский конструктор.
+   */
   public BasicCommand(String commandIdentifier, String description) {
     super(commandIdentifier, description);
   }
 
+  /**
+   * Простая обёртка над процессом выполнения отправки ответов телеграм-ботом, чтобы
+   * не приходилось постоянно засовывать отправку в try-catch блоки с типичной логикой обработки.
+   */
+  protected <T extends Serializable, Method extends BotApiMethod<T>> T send(AbsSender absSender, Method method)  {
+    try {
+      return absSender.execute(method);
+    } catch (TelegramApiException e) {
+      log.error("В ходе отправки сообщения произошла ошибка: ", e);
+      throw new RuntimeException("В ходе отправки сообщения произошла ошибка", e);
+    }
+  }
 }
