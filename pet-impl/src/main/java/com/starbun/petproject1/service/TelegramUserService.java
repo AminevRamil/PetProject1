4c9e24dd9c22d8f9fb54632de4d32c2a9ea332c2
Ramil Aminev <Aminev@lanit.ru>
2022-11-08 01:23:09 +0400

    Обновление сервиса пользователей.

--- pet-impl/src/main/java/com/starbun/petproject1/service/TelegramUserService.java
+++ pet-impl/src/main/java/com/starbun/petproject1/service/TelegramUserService.java
@@ -1,9 +1,14 @@
 package com.starbun.petproject1.service;
 
+import com.starbun.petproject1.dto.State;
 import com.starbun.petproject1.dto.TelegramUserDto;
+import com.starbun.petproject1.dto.UserStateDto;
 import com.starbun.petproject1.entity.TelegramUser;
+import com.starbun.petproject1.entity.UserState;
 import com.starbun.petproject1.mapper.TelegramUserMapper;
+import com.starbun.petproject1.mapper.UserStateMapper;
 import com.starbun.petproject1.repository.TelegramUsersRepository;
+import com.starbun.petproject1.repository.UserStateRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
@@ -11,29 +16,34 @@ import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.User;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class TelegramUserService {
 
-  private final TelegramUsersRepository repository;
+  private final TelegramUsersRepository usersRepository;
 
-  private final TelegramUserMapper mapper;
+  private final UserStateRepository userStateRepository;
+
+  private final TelegramUserMapper telegramUserMapper;
+
+  private final UserStateMapper userStateMapper;
 
   /**
    * Сохранение информации о новом пользователе
    */
   @Transactional
   public TelegramUserDto registerUser(User user) {
-    if (repository.existsTelegramUsersByTgId(user.getId())) {
-      TelegramUser existingUser = repository.findFirstByTgId(user.getId());
+    if (usersRepository.existsTelegramUsersByTgId(user.getId())) {
+      TelegramUser existingUser = usersRepository.findFirstByTgId(user.getId());
       updateInfoAboutUser(user, existingUser);
-      return mapper.mapToTelegramUserDto(existingUser);
+      return telegramUserMapper.mapToTelegramUserDto(existingUser);
     }
-    TelegramUser telegramUser = mapper.mapToNewTelegramUser(user);
+    TelegramUser telegramUser = telegramUserMapper.mapToNewTelegramUser(user);
     log.info("Регистрация пользователя {}", telegramUser);
-    return mapper.mapToTelegramUserDto(repository.save(telegramUser));
+    return telegramUserMapper.mapToTelegramUserDto(usersRepository.save(telegramUser));
   }
 
   /**
@@ -52,5 +62,29 @@ public class TelegramUserService {
       existingUser.setLastName(user.getLastName());
     }
     existingUser.setIsActual(true);
+  }
+
+  /**
+   * Выгрузка текущего состояния работы пользователя с ботом и создание начального состояния, если
+   * пользователь ранее с ботом не работал.
+   * @param tgId идентификатор пользователя
+   */
+  @Transactional
+  public UserStateDto getCurrentStatus(Long tgId) {
+    Optional<UserState> userStateOpt = userStateRepository.findById(tgId);
+    UserState userState = userStateOpt.orElse(createBeginState(tgId));
+    return userStateMapper.toDto(userState);
+  }
+
+  /**
+   * Создание новой записи о состоянии бота для конкретного пользователя
+   * @param tgId идентификатор пользователя
+   * @return состояние пользователя
+   */
+  private UserState createBeginState(Long tgId) {
+    UserState newUserState = new UserState();
+    newUserState.setTgId(tgId);
+    newUserState.setCurrentState(State.BEGIN);
+    return userStateRepository.save(newUserState);
   }
 }
