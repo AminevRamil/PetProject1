05c108a32f2eff3377f750ffbeec04b43c25be48
Ramil Aminev <Aminev@lanit.ru>
2022-11-08 01:23:39 +0400

    Обновление стартовой команды для начала работы с ботом.

--- pet-impl/src/main/java/com/starbun/petproject1/bot/command/StartCommand.java
+++ pet-impl/src/main/java/com/starbun/petproject1/bot/command/StartCommand.java
@@ -1,23 +1,25 @@
 package com.starbun.petproject1.bot.command;
 
 import com.starbun.petproject1.dto.TelegramUserDto;
+import com.starbun.petproject1.dto.UserStateDto;
 import com.starbun.petproject1.exception.NoImplementationException;
 import com.starbun.petproject1.exception.TelegramApiMismatchException;
 import com.starbun.petproject1.service.TelegramUserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.bots.AbsSender;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+@Slf4j
 @Component
 public class StartCommand extends BasicCommand {
 
   private final TelegramUserService telegramUserService;
 
   public StartCommand(TelegramUserService telegramUserService) {
-    super("start", "Старт");
+    super("start", "Старт или перезапуск бота");
     this.telegramUserService = telegramUserService;
   }
 
@@ -26,15 +28,9 @@ public class StartCommand extends BasicCommand {
     switch (chat.getType()) {
       case "private" -> {
         TelegramUserDto telegramUser = telegramUserService.registerUser(user);
-        SendMessage message = new SendMessage();
-        message.enableMarkdown(true);
-        message.setChatId(chat.getId().toString());
-        message.setText("Стартуем, " + telegramUser.getActualUsername() + "!");
-        try {
-          absSender.execute(message);
-        } catch (TelegramApiException e) {
-          //логируем сбой Telegram Bot API, используя commandName и userName
-        }
+        UserStateDto currentState = telegramUserService.getCurrentStatus(telegramUser.getTgId());
+        SendMessage message = createGreetingsMessage(chat, telegramUser);
+        send(absSender, message);
       }
       case "group", "channel", "supergroup" -> {
         throw new NoImplementationException("Ещё нет обработки команды /start для чата типа " + chat.getType());
@@ -43,6 +39,16 @@ public class StartCommand extends BasicCommand {
         throw new TelegramApiMismatchException("Сообщение пришло из чата неизвестного типа: " + chat.getType());
       }
     }
+  }
 
+  /**
+   * Создание начального приветственного сообщения
+   */
+  private SendMessage createGreetingsMessage(Chat chat, TelegramUserDto telegramUser) {
+    SendMessage message = new SendMessage();
+    message.enableMarkdown(true);
+    message.setChatId(chat.getId().toString());
+    message.setText("Стартуем, " + telegramUser.getActualUsername() + "!");
+    return message;
   }
 }
