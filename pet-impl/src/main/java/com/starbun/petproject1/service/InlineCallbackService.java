8b5e58594f33ec0b80f3c5cdcebebf899bce5032
Ramil Aminev <Aminev@lanit.ru>
2022-11-21 10:41:24 +0400

    Добавлен сервис для работы с информацией получаемой из кнопки.

---
+++ pet-impl/src/main/java/com/starbun/petproject1/service/InlineCallbackService.java
@@ -0,0 +1,22 @@
+package com.starbun.petproject1.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.starbun.petproject1.dto.InlineButtonInfo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class InlineCallbackService {
+
+  private final ObjectMapper jsonMapper;
+
+  public InlineButtonInfo getInlineData(String data){
+    try {
+      return jsonMapper.readValue(data, InlineButtonInfo.class);
+    } catch (JsonProcessingException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
