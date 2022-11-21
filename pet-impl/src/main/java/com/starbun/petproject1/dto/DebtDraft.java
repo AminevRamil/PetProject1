8e0750ef2e970844d365a819c84161a6aae19af1
Ramil Aminev <Aminev@lanit.ru>
2022-11-21 11:20:57 +0400

    [Долг] Добавлена дата погашения долга в черновик долга, и задано значение статуса погашения по умолчанию.

--- pet-impl/src/main/java/com/starbun/petproject1/dto/DebtDraft.java
+++ pet-impl/src/main/java/com/starbun/petproject1/dto/DebtDraft.java
@@ -1,13 +1,19 @@
 package com.starbun.petproject1.dto;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.time.LocalDate;
 
 @Data
+@EqualsAndHashCode(callSuper = false)
 public class DebtDraft extends DraftObject {
 
   private String debtor;
 
   private String deptSubject;
 
-  private Boolean redemptionStatus;
+  private LocalDate debtDate;
+
+  private Boolean redemptionStatus = false;
 }
