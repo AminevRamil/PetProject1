7986d146cfa276061526588ac87e5e065ef516db
Ramil Aminev <Aminev@lanit.ru>
2022-12-20 01:19:29 +0400

    [Мета] Наброски контроллера, управляющего жизненным циклом комманд-прототипов

---
+++ pet-impl/src/main/java/com/starbun/petproject1/util/CommandsLifeCycleController.java
@@ -0,0 +1,26 @@
+package com.starbun.petproject1.util;
+
+import com.starbun.petproject1.bot.command.BasicCommand;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class CommandsLifeCycleController {
+
+  private final Map<Long, BasicCommand> userToCommandMap = new HashMap<>();
+
+  public BasicCommand getCommandByUserId(Long userId) {
+    /**
+     * TODO
+     * Вернуть либо новую команду (но тогда надо передать требуемый тип команды для создания на лету),
+     * либо вернуть нулл и во вне обработать через создание команды
+     */
+    return userToCommandMap.getOrDefault(userId, null);
+  }
+
+  public void storeNewCommand(BasicCommand command) {
+
+  }
+}
