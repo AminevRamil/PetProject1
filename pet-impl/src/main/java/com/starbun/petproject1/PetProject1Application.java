85fc96aa189706a6e252ff56861360a3f970d6a7
Ramil Aminev <Aminev@lanit.ru>
2022-12-22 00:26:14 +0400

    [Мета] Включен планировщик и убрано излишнее логирование.

--- pet-impl/src/main/java/com/starbun/petproject1/PetProject1Application.java
+++ pet-impl/src/main/java/com/starbun/petproject1/PetProject1Application.java
@@ -2,9 +2,9 @@ package com.starbun.petproject1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class PetProject1Application {
 
