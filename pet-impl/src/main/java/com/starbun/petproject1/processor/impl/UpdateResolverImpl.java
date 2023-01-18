f2639241d082fcefc7df30d32b6c5262b91b081a
Ramil Aminev <Aminev@lanit.ru>
2023-01-18 23:19:59 +0400

    [Мета] Удалён закомментированный код

--- pet-impl/src/main/java/com/starbun/petproject1/processor/impl/UpdateResolverImpl.java
+++ pet-impl/src/main/java/com/starbun/petproject1/processor/impl/UpdateResolverImpl.java
@@ -32,22 +32,11 @@ public class UpdateResolverImpl implements UpdateResolver {
   @Override
   public void process(AbsSender absSender, Update update) {
     UpdateExtended updateExt = new UpdateExtended(update);
-
     BasicProcessor<? extends BotApiObject> basicProcessor = processorMap.get(updateExt.getUpdateType());
 
     if (basicProcessor == null) {
       throw new NoImplementationException("Нет реализации процессора сообщений типа " + updateExt.getUpdateType());
     }
     basicProcessor.process(absSender, updateExt.getContent());
-//    switch (updateExt.getUpdateType()) {
-//      case MESSAGE -> messageProcessor.process(absSender, update.getMessage());
-//      case CALLBACK_QUERY -> callbackQueryProcessor.process(absSender, update.getCallbackQuery());
-//      case INLINE_QUERY, CHOSEN_INLINE_QUERY, EDITED_MESSAGE, CHANNEL_POST,
-//        EDITED_CHANNEL_POST, SHIPPING_QUERY, PRE_CHECKOUT_QUERY, POLL, POLL_ANSWER,
-//          MY_CHAT_MEMBER, CHAT_MEMBER, CHAT_JOIN_REQUEST -> {
-//        throw new NoImplementationException("Нет реализации процессора сообщений типа " + updateExt.getUpdateType());
-//      }
-//      default -> throw new IllegalArgumentException("Пришёл неизвестный тип сообщения от телеги. Пора обновлять апи");
-//    }
   }
 }
